67c5fbc7850ad7f08dcae33a09acea0b78f8a4ed
Vas Sferd <vassferd@gmail.com>
2023-03-16 23:43:22 +0300

    Небольшой рефакторинг

--- index.php
+++ index.php
@@ -1,55 +1,54 @@
-<!DOCTYPE html>
-<html>
+<?php
+// Подключаемся к базе данных PostgreSQL
+$dbname = getenv('PG_DBNAME');
+$user = getenv('PG_USER');
+$pass = getenv('PG_PASS');
+$host = getenv('PG_HOST');
+$port = getenv('PG_PORT');
 
-<head>
-    <title>Movie Reviews</title>
-</head>
+$pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
 
-<body>
-    <?php
+// Проверяем, авторизован ли пользователь
+if(isset($_SESSION['user_id'])){
+  $user_id = $_SESSION['user_id'];
+  $user_role = $_SESSION['user_role'];
 
-    // // Выборка названий таблиц
-    // $query = $connection->query("SELECT name FROM sqlite_master WHERE type='table'");
-
-    // // Вывод результатов запроса
-    // while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-    //     echo $row['name'] . '<br>';
-    // }
-
-    //Проверяем, авторизован ли пользователь
-    if (isset($_SESSION['user_id'])) {
-        $user_id = $_SESSION['user_id'];
-        $user_role = $_SESSION['user_role'];
-
-        //Выводим приветствие и ссылку на создание нового обзора, если пользователь администратор или модератор
-        if ($user_role == 'admin' || $user_role == 'moderator') {
-            echo "<p>Welcome, User #$user_id. <a href='add_review.php'>Create a new review</a>.</p>";
-        } else {
-            echo "<p>Welcome, User #$user_id.</p>";
-        }
-
-        //Выводим кнопку выхода из аккаунта
-        echo "<form action='logout.php' method='POST'><input type='submit' value='Log out'></form>";
-    } else {
-        //Выводим ссылки на авторизацию и регистрацию для не авторизованных пользователей
-        echo "<p><a href='login.php'>Log in</a> to post reviews and comments, or <a href='register.php'>register</a> if you're new here.</p>";
-    }
-
-    //Запрос на получение всех обзоров фильмов из базы данных
-    $query = "SELECT * FROM 'reviews'";
-    $stmt = $connection->query($query);
-    $reviews = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    //Отображаем каждый обзор на главной странице
-    foreach ($reviews as $review) {
-        echo "<h2><a href='review.php?id={$review['id']}'>{$review['title']}</a></h2>";
-        echo "<p>By {$review['author']} at " . date('F j, Y', strtotime($review['date'])) . "</p>";
-        echo "<img src='{$review['poster']}' alt='{$review['title']}'>"; // для отображения постера фильма
-        echo "<p>{$review['summary']}</p>";
-        echo "<hr>";
-    }
-    unset($connection)
+  // Выводим приветствие и ссылку на создание нового обзора, если пользователь администратор или модератор
+  if($user_role == 'admin' || $user_role == 'moderator'){
     ?>
-</body>
+      <p>Welcome, User #<?= $user_id ?>. <a href='add_review.php'>Create a new review</a>.</p>
+    <?php
+  }else{
+    ?>
+      <p>Welcome, User #<?= $user_id ?>.</p>
+    <?php
+  }
 
-</html>
+  // Выводим кнопку выхода из аккаунта
+  ?>
+    <form action='logout.php' method='POST'><input type='submit' value='Log out'></form>
+  <?php
+}else{
+  // Выводим ссылки на авторизацию и регистрацию для не авторизованных пользователей
+  ?>
+    <p><a href='login.php'>Log in</a> to post reviews and comments, or <a href='register.php'>register</a> if you're new here.</p>
+  <?php
+}
+
+// Запрос на получение всех обзоров фильмов из базы данных
+$query = "SELECT * FROM reviews";
+$stmt = $pdo->prepare($query);
+$stmt->execute();
+$reviews = $stmt->fetchAll();
+
+// Отображаем каждый обзор на главной странице
+foreach($reviews as $review){
+  ?>
+    <h2><a href='review.php?id=<?= $review['id'] ?>'><?= $review['title'] ?></a></h2>
+    <p>By <?= $review['author'] ?> at <?= date('F j, Y', strtotime($review['date'])) ?></p>
+    <img src='<?= $review['poster'] ?>' alt='<?= $review['title'] ?>'>
+    <p><?= $review['summary'] ?></p>
+    <hr>
+  <?php
+}
+?>
